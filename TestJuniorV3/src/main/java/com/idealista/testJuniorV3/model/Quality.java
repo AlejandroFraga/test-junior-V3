e4e0944d12a6315f84d0ca91b79bd1cf11bf0dc4
Alejandro Fraga Cimadevila <alex_fraga95@hotmail.com>
2020-11-06 13:35:48 +0100

    Gitignore updated

--- TestJuniorV3/src/main/java/com/idealista/testJuniorV3/model/Quality.java
+++
@@ -1,5 +0,0 @@
-package com.idealista.testJuniorV3.model;
-
-public enum Quality {
-	SD, HD, NONE;
-}
--- TestJuniorV3/src/main/java/com/idealista/testJuniorV3/model/Typology.java
+++
@@ -1,5 +0,0 @@
-package com.idealista.testJuniorV3.model;
-
-public enum Typology {
-	CHALET, FLAT, GARAGE, NONE;
-}
